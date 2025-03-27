2c1b486880d6a6a90e8c740fdc0976c890513c6f
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-03-27 18:48:06 -0400

    Contains Duplicate.py

---
+++ Contains Duplicate.py
@@ -0,0 +1,3 @@
+# https://www.designgurus.io/viewer/document/grokking-the-coding-interview/63d8efe7f8694f3655d60712class Solution:
+    def containsDuplicate(self, nums):
+        return len(nums) != len(set(nums))
